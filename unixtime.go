ea678a2ac793ef8a58202f18b19e89c6a3eea4eb
Vetti <vetti.ch@ya.ru>
2019-09-27 14:31:04 +0500

    added Token rpc

--- unixtime.go
+++ unixtime.go
@@ -2,13 +2,13 @@ package gapi
 
 import "time"
 
-func NewUnixTime(t time.Time) UnixTime {
-	return UnixTime{
+func NewUnixTime(t time.Time) *UnixTime {
+	return &UnixTime{
 		Sec:  t.Unix(),
 		Nsec: t.UnixNano(),
 	}
 }
 
-func (ut UnixTime) Time() time.Time {
+func (ut *UnixTime) Time() time.Time {
 	return time.Unix(ut.Sec, ut.Nsec)
 }
